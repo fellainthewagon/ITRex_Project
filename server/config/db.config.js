1f903991ee261ae85889e8f18da24c38c39884fa
fellainthewagon <fellainthewagon@gmail.com>
2021-08-25 20:22:52 +0300

    docker

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  DB: "clinic",
-  USER: "fella",
-  PASSWORD: "fella",
-  HOST: "127.0.0.1",
-  PORT: 3306,
+  DB: process.env.DB_NAME || "clinic",
+  USER: process.env.DB_USER || "fella",
+  PASSWORD: process.env.DB_PASSWORD || "fella",
+  HOST: process.env.DB_HOST || "127.0.0.1",
+  PORT: process.env.DB_PORT || 3306,
   dialect: "mysql",
   pool: {
     max: 5,
