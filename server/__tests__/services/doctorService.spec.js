9533dede0687459eb2a3aadf39f46e86a03e788a
SergeyKosovich <kosovichsergey322@gmail.com>
2021-09-15 11:21:48 +0300

    fix tests

--- server/__tests__/services/doctorService.spec.js
+++ server/__tests__/services/doctorService.spec.js
@@ -1,13 +1,27 @@
 const doctorService = require("../../src/components/doctor/doctorService");
 const doctorStorage = require("../../src/components/repositories/doctorStorage");
-
-doctorStorage.findById = jest.fn();
 const id = 2;
+const object = { doctor_id: "1" };
+const specialization = { specialization: "dentist" };
+doctorStorage.findById = jest.fn(() => object);
+doctorStorage.findSpecialization = jest.fn(() => specialization);
 
-describe("'doctorStorage' class", () => {
-  it("'findById' method", async () => {
-    doctorService.getDoctorId(id);
+describe("'doctorService' class", () => {
+  it("'getDoctorId' method", async () => {
+    const doctor = await doctorService.getDoctorId(id);
     expect(doctorStorage.findById).toHaveBeenCalledTimes(1);
     expect(doctorStorage.findById).toBeCalledWith(id);
+    expect(doctor).toEqual({ doctor_id: "1" });
+  });
+});
+
+describe("'doctorService' class", () => {
+  it("'getDoctorSpecialization' method", async () => {
+    const doctorSpecialization = await doctorService.getDoctorSpecialization(
+      id
+    );
+    expect(doctorStorage.findSpecialization).toHaveBeenCalledTimes(1);
+    expect(doctorStorage.findSpecialization).toBeCalledWith(id);
+    expect(doctorSpecialization).toEqual({ specialization: "dentist" });
   });
 });
